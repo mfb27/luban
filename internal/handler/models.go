5d574eebba665862a044d1738e1b2e9feb8c9696
mengfanbing <mengfanbingyx@163.com>
2026-03-31 00:34:04 +0800

    feat: filter models to only show active ones in getModels handler

--- internal/handler/models.go
+++ internal/handler/models.go
@@ -8,7 +8,7 @@ import (
 
 func (a *App) getModels(c *gin.Context) {
 	var models []model.Model
-	if err := a.db.Order("updated_at desc").Find(&models).Error; err != nil {
+	if err := a.db.Where("status = ?", "active").Order("updated_at desc").Find(&models).Error; err != nil {
 		response.NewResponseHelper(c).Error(response.CodeDatabaseError, err.Error())
 		return
 	}
